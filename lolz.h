6c2f1ecc4b320d66fee25f1afdf8c31aa18cb4e9
Bharat Middha <bmiddha99@gmail.com>
2019-10-06 11:27:16 -0500

    Added configLolz to customize freq and speed

--- lolz.h
+++ lolz.h
@@ -11,7 +11,17 @@
 #include <stdarg.h>
 
 #define ARRAY_SIZE(foo) (sizeof(foo) / sizeof(foo[0]))
+
 const unsigned char codes[] = {39, 38, 44, 43, 49, 48, 84, 83, 119, 118, 154, 148, 184, 178, 214, 208, 209, 203, 204, 198, 199, 163, 164, 128, 129, 93, 99, 63, 69, 33};
+static double freq_h = 0.23, freq_v = 0.1;
+static int speed = 300;
+
+static void configLolz(double _freq_h, double _freq_v, int _speed)
+{
+    freq_h = (_freq_h != 0.0) ? _freq_h : freq_h;
+    freq_v = (_freq_v != 0.0) ? _freq_v : freq_v;
+    speed = (_speed != 0) ? _speed : speed;
+}
 
 static void find_escape_sequences(wint_t c, int *state)
 {
@@ -34,11 +44,10 @@ void lolcat(char *input)
 {
     int cc = -1, i, l = 0;
     int colors = isatty(STDOUT_FILENO);
-    double freq_h = 0.23, freq_v = 0.1;
 
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    double offx = (tv.tv_sec % 300) / 300.0;
+    double offx = (tv.tv_sec % speed) / (double)speed;
 
     i = 0;
     int escape_state = 0;
@@ -80,13 +89,13 @@ void lolcat(char *input)
     cc = -1;
 }
 
-
-void lolz (char *fmt, ...) {
-    char *buf = malloc (sizeof(fmt));
+void lolz(char *fmt, ...)
+{
+    char *buf = (char *)malloc(sizeof(fmt));
     va_list va;
-    va_start (va, fmt);
-    vsprintf (buf, fmt, va);
+    va_start(va, fmt);
+    vsprintf(buf, fmt, va);
     lolcat(buf);
-    va_end (va);
+    va_end(va);
     free(buf);
 }
